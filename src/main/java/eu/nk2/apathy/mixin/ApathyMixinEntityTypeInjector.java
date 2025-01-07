e3a606f9f50e6a636fdd15741f52bcde56612c12
Nik Kozlov <nikkoz893@gmail.com>
2025-01-07 19:00:36 +0100

    Error handler at create entity

--- src/main/java/eu/nk2/apathy/mixin/ApathyMixinEntityTypeInjector.java
+++ src/main/java/eu/nk2/apathy/mixin/ApathyMixinEntityTypeInjector.java
@@ -32,7 +32,13 @@ public abstract class ApathyMixinEntityTypeInjector<T extends Entity> implements
         SpawnReason reason,
         CallbackInfoReturnable<T> cir
     ) {
-        EntityType<T> entityType = (EntityType<T>) (Object) this;
-        if(customFactory != null) cir.setReturnValue(customFactory.create(entityType, world));
+        var entityType = (EntityType<T>) (Object) this;
+        var factory = customFactory != null ? customFactory : getFactory();
+
+        cir.setReturnValue(
+            entityType.isEnabled(world.getEnabledFeatures())
+                ? factory.create(entityType, world)
+                : null
+        );
     }
 }
